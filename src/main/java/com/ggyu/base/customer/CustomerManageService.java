f6930200807f0eb97594571bdf0d2a23c8c862c0
changha <chjeong@leevi.co.kr>
2023-02-20 00:22:00 +0900

    모듈 관계 설정 변경(build.gradle)

---
+++ src/main/java/com/ggyu/base/customer/CustomerManageService.java
@@ -0,0 +1,7 @@
+package com.ggyu.base.customer;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class CustomerManageService {
+}
---
+++ src/main/java/com/ggyu/base/test/Test.java
@@ -0,0 +1,21 @@
+package com.ggyu.base.test;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "test", catalog = "db_test")
+public class Test {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    private String name;
+
+    private LocalDateTime updatedAt;
+}
